de6ad2dcd3f2399a0501ae15a43b897d6732701f
get <michaelkinuthia708@gmail.com>
2026-06-05 21:58:20 +0300

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // Target your local Django backend
-const API_BASE = 'http://127.0.0.1:8000/api';
+const API_BASE = 'https://terrayield-predictor.onrender.com/api';
 
 export const fetchCropPrediction = async (lat, lon) => {
   try {
